b6a01abe05df7a4517c27ea536cf3bb451b7a269
Dean Netherton <dnetherton@dius.com.au>
2019-05-22 16:32:59 +1000

    Fix for merge example

--- src/examples/merge/example_1.js
+++ src/examples/merge/example_1.js
@@ -1,19 +1,19 @@
 import {merge} from '../..'
 
 async function* source1() {
-  yield await 1
-  yield await 2
-  yield await 3
-  yield await 4
-  yield await 5
+  yield await Promise.resolve(1)
+  yield await Promise.resolve(2)
+  yield await Promise.resolve(3)
+  yield await Promise.resolve(4)
+  yield await Promise.resolve(5)
 }
 
 async function* source2() {
-  yield await 'a'
-  yield await 'b'
-  yield await 'c'
-  yield await 'd'
-  yield await 'e'
+  yield await Promise.resolve('a')
+  yield await Promise.resolve('b')
+  yield await Promise.resolve('c')
+  yield await Promise.resolve('d')
+  yield await Promise.resolve('e')
 }
 
 async function main() {
