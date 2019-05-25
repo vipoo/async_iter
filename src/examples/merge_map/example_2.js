7bb334fb355c596d3c65bf5b515c7c58897791d8
Dean Netherton <dnetherton@dius.com.au>
2019-05-25 16:19:31 +1000

    Added sync version of mergeMap

---
+++ src/examples/merge_map/example_2.js
@@ -0,0 +1,14 @@
+import {forEach, mergeMap, map} from '../..'
+
+const source1 = [1, 2, 3, 4, 5]
+const source2 = ['a', 'b', 'c', 'd', 'e']
+
+async function main() {
+  await (source2
+    |> mergeMap(?, x => source1 |> map(?, i => `${i} - ${x}`))
+    |> forEach(?, console.log))
+
+  console.log('done....')
+}
+
+main()
--- src/merge_map.js
+++ src/merge_map.js
@@ -1,8 +1,15 @@
-import {asAsyncIterator} from './lib/get_iterator'
+import {syncType, asAsyncIterator} from './lib/get_iterator'
 
-export async function* mergeMap(source, fn) {
+export const mergeMap = syncType(syncMergeMap, asyncMergeMap)
+
+export async function* asyncMergeMap(source, fn) {
   source = await asAsyncIterator(source)
 
   for await (const item of source)
     yield* await asAsyncIterator(await fn(item))
+}
+
+export function* syncMergeMap(source, fn) {
+  for (const item of source)
+    yield* fn(item)
 }
