bb917e1289c3e45b38fa526137870dc73bb0d61d
Dean Netherton <dnetherton@dius.com.au>
2019-08-16 14:24:50 +1000

    Added docs for operator first

--- src/first.js
+++ src/first.js
@@ -1,5 +1,20 @@
 import {asAsyncIterator, syncType} from './lib/get_iterator'
 
+/**
+```
+import {first} from 'async_iter/pipeline/first' # pipeline version
+import {first} from 'async_iter/first' # conventional version
+```
+Returns the first item of the source iterable
+> Supports both **sync** and **async** iterations
+
+ * @param  {Iterable}         source          The source iteration
+ * @return {*} The first items
+ * @function
+ * @name first
+ * @memberof module:Operators
+ */
+
 export const first = syncType(syncFirst, asyncFirst)
 
 function syncFirst(source) {
