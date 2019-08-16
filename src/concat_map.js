4b8af068cfc529c369c2c8cd113df36a3aa0624e
Dean Netherton <dnetherton@dius.com.au>
2019-08-16 15:55:14 +1000

    Added docs for concatMap operator

--- src/concat_map.js
+++ src/concat_map.js
@@ -1,5 +1,22 @@
 import {syncType, asAsyncIterator} from './lib/get_iterator'
 
+/**
+```
+import {concatMap} from 'async_iter/pipeline/concat_map' # pipeline version
+import {concatMap} from 'async_iter/concat_map' # conventional version
+```
+Joins an inner iterable with the use of a mapping function.
+
+> Supports both **sync** and **async** iterations
+
+ * @param  {Iterable}         source        The source iteration
+ * @param {Function} fn A function called for each item in the source iteration and returns an iterable
+ * @return {F} An iterable that has flatten the inner iterable values
+ * @function
+ * @name concatMap
+ * @memberof module:Operators
+*/
+
 export const mergeMap = syncType(syncMergeMap, asyncMergeMap)
 
 export async function* asyncMergeMap(source, fn) {
