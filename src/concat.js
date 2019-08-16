491f9a32493e6410ba480fde379c089153c4140d
Dean Netherton <dnetherton@dius.com.au>
2019-08-16 13:40:56 +1000

    Add initial doc for concat

--- src/concat.js
+++ src/concat.js
@@ -1,5 +1,21 @@
 import {asAsyncIterator, syncType} from './lib/get_iterator'
 
+/**
+```
+import {concat} from 'async_iter/pipeline/concat' # pipeline version
+import {concat} from 'async_iter/concat' # conventional version
+```
+
+Combines multiple source iterations into a single iteration
+> Supports both **sync** and **async** iterations
+
+ * @param  {...Iterable}         sources        The source iterations
+ * @return {Iterable} An iterable that combines all values from the sources
+ * @function
+ * @name concat
+ * @memberof module:Operators
+*/
+
 export const concat = syncType(syncConcat, asyncConcat)
 
 function* syncConcat(...sources) {
