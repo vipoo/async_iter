ec097e6af7522a5730ee320eddd16ccc7d55e854
Dean Netherton <dnetherton@dius.com.au>
2019-08-16 15:38:47 +1000

    Added docs for merge operator

--- src/merge.js
+++ src/merge.js
@@ -1,5 +1,24 @@
 import {asAsyncIterator} from './lib/get_iterator'
 
+/**
+```
+import {merge} from 'async_iter/pipeline/merge' # pipeline version
+import {merge} from 'async_iter/merge' # conventional version
+```
+
+Merges the source iterables items into a single iterable.  Order is as they come.
+
+If all source iterables are stopped, then the returned iteration is stopped
+
+If the consumer stops the iteration, all source iterations are stopped.
+
+ * @param  {Iterable}         sources        The source iterations
+ * @return {Iterable} An iterable that combines all values from the sources
+ * @function
+ * @name merge
+ * @memberof module:Operators
+ */
+
 export async function* merge(...sources) {
   let done = sources.length
   const items = sources
