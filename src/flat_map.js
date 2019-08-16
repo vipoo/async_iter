639633062e5a50da4ef23aebedfd1283bddd6d54
Dean Netherton <dnetherton@dius.com.au>
2019-08-16 16:10:56 +1000

    Added docs for flatMap operator

--- src/flat_map.js
+++ src/flat_map.js
@@ -1,2 +1,10 @@
 import {concatMap} from './concat_map'
+
+/**
+ * Alias for concatMap
+ * @see also [concatMap](module-Operators.html#.concatMap)
+ * @memberof module:Operators
+ * @function
+ * @name flatMap
+ */
 export const flatMap = concatMap
