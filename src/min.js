439bf9affb329ba1d273364a629b28d5a919768b
Dean Netherton <dnetherton@dius.com.au>
2019-08-16 15:35:10 +1000

    Added docs for min operator

--- src/min.js
+++ src/min.js
@@ -1,6 +1,22 @@
 import {asAsyncIterator, syncType} from './lib/get_iterator'
 import {syncBy, syncByComp, syncValue, asyncBy, asyncByComp, asyncValue} from './lib/min_max_support'
 
+/**
+```
+import {min} from 'async_iter/pipeline/min' # pipeline version
+import {min} from 'async_iter/min' # conventional version
+```
+Returns the min value of the source iterable items
+> Supports both **sync** and **async** iterations
+
+ * @param {Iterable}         source          The source iteration
+ * @param {minMaxCallback=} fn An function to determine value order
+ * @return {*} The min item
+ * @function
+ * @name min
+ * @memberof module:Operators
+ */
+
 export const min = syncType(syncMin, asyncMin)
 
 const syncMinBy = (source, fn) => syncBy((a, b) => a > b, source, fn)
