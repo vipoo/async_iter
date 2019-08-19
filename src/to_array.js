7970e9c6f9b51de89138ddcce9b72da7b3bbc1c7
Dean Netherton <dnetherton@dius.com.au>
2019-08-19 13:49:22 +1000

    Added docs for to_array

--- src/to_array.js
+++ src/to_array.js
@@ -1,5 +1,22 @@
 import {asAsyncIterator, syncType} from './lib/get_iterator'
 
+/**
+```
+import {toArray} from 'async_iter/pipeline/to_array' # pipeline version
+import {toArray} from 'async_iter/to_array' # conventional version
+```
+ Converts an iterable source to an array
+
+ > Supports both **sync** and **async** iterations
+
+ * @param {Iterable} source   The source iteration
+ * @returns {Array}        An array containing all the items of the source iteration
+ * @name toArray
+ * @function
+ * @memberof module:Operators
+ * @name toArray
+ */
+
 export const toArray = syncType(syncToArray, asyncToArray)
 
 function syncToArray(source) {
