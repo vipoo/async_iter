d00619a0525d333049d49369d5325b0c9f68581a
Dean Netherton <dnetherton@dius.com.au>
2019-08-19 13:39:28 +1000

    Added docs for take_until

--- src/take_until.js
+++ src/take_until.js
@@ -1,5 +1,24 @@
 import {syncType, asAsyncIterator} from './lib/get_iterator'
 
+/**
+```
+import {takeUntil} from 'async_iter/pipeline/take_until' # pipeline version
+import {takeUntil} from 'async_iter/take_until' # conventional version
+```
+ Takes a number of items from the source iteration
+
+ > Supports both **sync** and **async** iterations
+
+ * @param {Iterable}      source    The source iteration
+ * @param {evaluateItem}  fn        Called for each item.  When function returns true, iteration is stopped.
+ * @returns {Iterable} A set containing all items of source, upto when fn returns true (non inclusive)
+ * @name takeUntil
+ * @function
+ * @example
+ * @memberof module:Operators
+ * @name takeUntil
+ */
+
 export const takeUntil = syncType(syncTakeUntil, asyncTakeUntil)
 
 function* syncTakeUntil(source, fn) {
